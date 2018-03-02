82873e8ce0ffaaac29d497e17db7114f62a43320
Pabloac35 <pabloac35@gmail.com>
2018-03-02 19:03:24 +0100

    Hello World modified

--- src/Prueba.java
+++ src/Prueba.java
@@ -3,6 +3,10 @@ public class Prueba {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		printHelloWorld();
+	}
+
+	private static void printHelloWorld() {
 		System.out.println("Hello World");
 	}
 
